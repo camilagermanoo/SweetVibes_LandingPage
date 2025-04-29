c6b9a4ae06c7b0d84b5330352913d0873214c7a5
Camila Germano Rodrigues <c.germano@iesb.edu.br>
2025-04-29 09:57:54 -0300

    adicionar carrossel

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,19 +1,3 @@
-const slide = document.querySelector('.carousel-slide');
-const images = document.querySelectorAll('.carousel-slide img');
-
-let counter = 0;
-const size = 370; 
-
-slide.style.width = `${size * images.length}px`;
-
-setInterval(() => {
-    counter++;
-    if (counter >= images.length) {
-        counter = 0;
-    }
-    slide.style.transform = `translateX(${-size * counter}px)`;
-}, 3000);
-
 $(document).ready(function() {
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active')
